70780bc6c6716e840c5f4aa9557b406452f12aae
natolij8585 <anatolij8585@bk.ru>
2022-11-29 13:04:57 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let string = n.toString();
+  if (n > 0) {
+  let reversed = string.split('').reverse().join('');
+    return reversed;
+  } else {
+    string = string.substring(1);
+    return reversed = string.split('').reverse().join('');
+  }
 }
